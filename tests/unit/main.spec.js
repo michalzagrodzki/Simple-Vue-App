205ed06c256b21122b01164443295fac9a4afc4b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-01 22:02:49 +0200

    Add additional cases to component Main tests

--- tests/unit/main.spec.js
+++ tests/unit/main.spec.js
@@ -2,9 +2,61 @@ import { mount } from '@vue/test-utils'
 import Main from '@/components/Main.vue'
 
 const wrapper = mount(Main)
-wrapper.setData({title: String})
+wrapper.setData (
+	{ title: String,
+    message: {
+      title: String,
+      subtitle: String
+    },
+    products: Array,
+    productAction: {
+      button: String
+    },
+    contact: {
+      title: String,
+      subtitle: String,
+      button: String
+    },
+    sections: {
+      message: String,
+      products: String,
+      contact: String
+    }
+  }
+)
+
 describe('Main', () => {
-  it('should contain title', () => { 
+  it('should contain page title', () => { 
   	expect(wrapper.vm.title).toBe(String)
+  })
+  it('should contain message title', () => { 
+    expect(wrapper.vm.message.title).toBe(String)
+  })
+  it('should contain message subttile', () => { 
+    expect(wrapper.vm.message.subtitle).toBe(String)
+  })
+  it('should contain products array', () => { 
+    expect(wrapper.vm.products).toBe(Array)
+  })
+  it('should contain product action button text', () => { 
+    expect(wrapper.vm.productAction.button).toBe(String)
+  })
+  it('should contain contact title', () => { 
+    expect(wrapper.vm.contact.title).toBe(String)
+  })
+  it('should contain contact subtitle', () => { 
+    expect(wrapper.vm.contact.subtitle).toBe(String)
+  })
+  it('should contain contact button', () => { 
+    expect(wrapper.vm.contact.button).toBe(String)
+  })
+  it('should contain message section text', () => { 
+    expect(wrapper.vm.sections.message).toBe(String)
+  })
+  it('should contain products section text', () => { 
+    expect(wrapper.vm.sections.products).toBe(String)
+  })
+  it('should contain contact section text', () => { 
+    expect(wrapper.vm.sections.contact).toBe(String)
   })
 })
