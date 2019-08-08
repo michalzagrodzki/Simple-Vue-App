209a27868b2df60caf2147a61b85224c561989dc
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-08 21:18:06 +0200

    Add test for Item component

---
+++ tests/unit/item.spec.js
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import Item from '@/components/Item.vue'
+
+const wrapper = mount(Item)
+wrapper.setData (
+	{ title: String,
+    description: String,
+    details: {
+      client: String,
+      services: String,
+      year: String,
+      link: String
+    },
+    images: Array
+  }
+)
+
+describe('Item', () => {
+  it('should contain product title', () => { 
+  	expect(wrapper.vm.title).toBe(String)
+  })
+  it('should contain product description', () => { 
+    expect(wrapper.vm.description).toBe(String)
+  })
+  it('should contain images array', () => { 
+    expect(wrapper.vm.images).toBe(Array)
+  })
+  it('should contain details of client', () => { 
+    expect(wrapper.vm.details.client).toBe(String)
+  })
+  it('should contain details of service', () => { 
+    expect(wrapper.vm.details.services).toBe(String)
+  })
+  it('should contain details of year', () => { 
+    expect(wrapper.vm.details.year).toBe(String)
+  })
+  it('should contain details of link', () => { 
+    expect(wrapper.vm.details.link).toBe(String)
+  })
+})
