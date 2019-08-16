a6363970514c8fdc993da576479f171af3356eaf
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-16 13:44:00 +0200

    Create outline for item template

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -2,8 +2,27 @@
   <div>
     <nav>Nav elements</nav>
     <section>
-      <div class="item-head-section">
-        <h1>{{ title }}</h1>
+      <div>
+        <div class="item-images-column">
+          <div v-for="image in images" class="item-image-item">
+            <img src="image" />
+            <p>{{ image.description }}</p>
+          </div>
+        </div>
+        <div class="item-description-section">
+          <h1>{{ title }}</h1>
+          <p>{{ description }}</p>
+          <div class="item-details">
+            <em>Client</em>
+            <p>{{ details.client }}</p>
+            <em>Services</em>
+            <p>{{ details.services }}</p>
+            <em>Year</em>
+            <p>{{ details.year }}</p>
+            <em>Link</em>
+            <p>{{ details.link }}</p>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -58,4 +77,36 @@ export default {
 @import '../assets/SCSS/colors.scss';
 @import '../assets/SCSS/fonts.scss';
 @import '../assets/SCSS/variables.scss';
+
+.item-images-column {
+
+}
+
+.item-image-item {
+
+  img {
+
+  }
+}
+
+.item-description-section {
+  h1 {
+
+  }
+
+  p {
+
+  }
+}
+
+.item-details {
+
+  em {
+
+  }
+
+  p {
+
+  }
+}
 </style>
