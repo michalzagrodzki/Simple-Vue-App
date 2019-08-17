f581f5d2f9f5bad07e07ab3bd910574f750718f9
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-17 17:32:49 +0200

    Outline template in Contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,11 +4,32 @@
     <section>
       <div class="contact-head-section">
         <h1>{{ title }}</h1>
-        <h2>{{ caption.subtitle }}</h2>
       </div>
     </section>
     <section>
       <div class="contact-form-section">
+        <h2>{{ caption.subtitle }}</h2>
+        <h4>{{ caption.text }}</h4>
+        <form>
+          <input type="text" placeholder="your name">
+          <input type="text" placeholder="your email">
+          <input type="text" placeholder="your message">
+          <button class="submit-button">{{ submitText }}</button>
+        </form>
+      </div>
+    </section>
+    <section>
+      <div class="contact-address-section">
+        <h2>{{ contact.title }}</h2>
+        <p>{{ contact.street }}</p>
+        <p>{{ contact.city }}</p>
+        <p>{{ contact.postcode }}</p>
+        <div class="contact-details">
+          <em>Email:</em>
+          <p>{{ contact.email}}</p>
+          <em>Phone:</em>
+          <p>{{ contact.phone}}</p>
+        </div>
       </div>
     </section>
   </div>
@@ -51,9 +72,38 @@ export default {
 
 .contact-head-section {
 
+  h1 {
+    
+  }
 }
 
 .contact-form-section {
 
+  h2 {
+
+  }
+  h4 {
+
+  }
+}
+
+.contact-address-section {
+
+  h2 {
+
+  }
+  p {
+
+  }
+}
+
+.contact-details {
+
+  em {
+
+  }
+  p {
+
+  }
 }
 </style>
