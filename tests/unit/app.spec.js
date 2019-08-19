77143d8d12c9231f974f2e27978ea76726a5fa67
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-19 12:23:29 +0200

    Add test for app component

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,17 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import App from '@/App.vue'
+import Router from 'vue-router'
+import routes from '@/router/router.js'
+
+const localVue = createLocalVue()
+localVue.use(Router)
+
+describe('App', () => {
+  it('should render correctly', () => {
+    const testRoutes = routes.options.routes
+    const testRouter = new Router({ testRoutes });
+    const wrapper = mount(App, { router: testRouter });
+    expect(wrapper.vm).toBeDefined();
+    expect(wrapper.element).toBeDefined();
+  })
+})
